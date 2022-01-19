48e2f04eea03e56a2bf4f3d32fc7f71c3ffdb0ca
Cuihuabin <Nick@trackview.co>
2022-01-19 13:44:18 +0800

    空指针报错

--- emoji/src/main/java/com/vanniktech/emoji/EmojiPopup.java
+++ emoji/src/main/java/com/vanniktech/emoji/EmojiPopup.java
@@ -307,13 +307,13 @@ import static com.vanniktech.emoji.Utils.checkNotNull;
     emojiResultReceiver.setReceiver(null);
 
     if (originalImeOptions != -1) {
-      editText.setImeOptions(originalImeOptions);
-      final InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-      if (inputMethodManager != null) {
-        inputMethodManager.restartInput(editText);
+      if(editText!=null){
+        editText.setImeOptions(originalImeOptions);
+        final InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+          inputMethodManager.restartInput(editText);
+        }
       }
-
       if (SDK_INT >= O) {
         final AutofillManager autofillManager = context.getSystemService(AutofillManager.class);
         if (autofillManager != null) {
